a3e88a66b2de118dcc97de41c346a0ef573cfba9
dyingSatyr <grabovacb86@gmail.com>
2021-01-06 13:37:17 +0100

    x

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 // import App from './App';
 
 function Hello() {
-    return <h1>Hello 4</h1>;
+    return <h1>Hello 5</h1>;
 }
 
 ReactDOM.render(<Hello />, document.getElementById('root'));
