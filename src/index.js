5b53021f2b611beeea9ea9b622ebb2a59d0527bd
dyingSatyr <grabovacb86@gmail.com>
2021-01-06 14:57:56 +0100

    inline and external css

--- src/index.js
+++ src/index.js
@@ -1,9 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // import App from './App';
+import './index.css';
 
-function Hello() {
-    return <h1>Hello 5</h1>;
+function App() {
+    return (
+        <section className="booklist">
+            <Book />
+            <Book />
+            <Book />
+            <Book />
+            <Book />
+        </section>
+    );
 }
 
-ReactDOM.render(<Hello />, document.getElementById('root'));
+const Book = () => {
+    return (
+        <article className="book">
+            <BookCover />
+            <BookTitle />
+            <BookAuthor />
+        </article>
+    );
+};
+
+const BookTitle = () => <h1 style={styles.title}>Book title</h1>;
+const BookAuthor = () => <h4 style={styles.author}>Douglas Adams</h4>;
+const BookCover = () => <img src="assets/hgtg.jpg" alt="HGTG" />;
+
+// To inject html elements in each other via arrow functions you must use createElement
+// in the innertext argument, which gets messy really fast
+// const App = () => React.createElement('h1', {}, 'Inner text');
+
+const styles = {
+    title: {
+        color: '#333333',
+        marginTop: '15px',
+    },
+    author: {
+        color: '#777',
+        marginTop: '5px',
+        fontSize: '13px',
+    },
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
